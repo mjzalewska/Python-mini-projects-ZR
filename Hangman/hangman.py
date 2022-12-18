35cc99d3840daa5f8a7b60f7163821067b08c6b9
magdazalewska <m.jarzebska.zalewska@gmail.com>
2022-12-18 17:17:18 +0100

    Added basic logic for the hangma game

---
+++ Hangman/hangman.py
@@ -0,0 +1,68 @@
+# wrong guess - dodaje element hangmana i rysuje od razu
+
+# powinien tak długo pytać o litery aż albo się wypełnią wszystkie albo zgadanie słowo!!
+# jak się wszystko wypełni to gra stop
+
+
+import requests
+
+api_url = 'https://api.api-ninjas.com/v1/randomword'
+key = 'MFh/MWtikknsaXiJKQNa8Q==qLDPv9767gHid06X'
+response = requests.get(api_url, params='noun', headers={'X-Api-Key': key})
+
+secret_word = response.json()['word']
+print(secret_word)
+
+print("""
+                                                                             
+ _|    _|                                                                    
+ _|    _|    _|_|_|  _|_|_|      _|_|_|  _|_|_|  _|_|      _|_|_|  _|_|_|    
+ _|_|_|_|  _|    _|  _|    _|  _|    _|  _|    _|    _|  _|    _|  _|    _|  
+ _|    _|  _|    _|  _|    _|  _|    _|  _|    _|    _|  _|    _|  _|    _|  
+ _|    _|    _|_|_|  _|    _|    _|_|_|  _|    _|    _|    _|_|_|  _|    _|  
+                                     _|                                      
+                                 _|_|                                        
+""")
+def find_index(word, char):
+    return [idx for idx, letter in enumerate(word) if letter == char]
+
+
+greeting = "Hello! Let's play hangman. Can you guess the secret word?"
+print('*' * len(greeting))
+print(greeting)
+print('*' * len(greeting))
+
+hidden_word = ['*'] * len(secret_word)
+print(f"The word I'm thinking of has {len(secret_word)} letters: {''.join(hidden_word)}")
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+game_on = True
+
+while hidden_word != secret_word:
+    print(f"Give me your letter: ")
+    player_guess = input().casefold()
+    if player_guess not in alphabet:
+        print("Sorry, this is not a letter! Please try again!")
+    else:
+        if player_guess in secret_word:
+            print("That's right!")
+            for s_index in find_index(secret_word, player_guess):
+                hidden_word[s_index] = player_guess
+                print(f"The secret word is now: {''.join(hidden_word)}") ## musi zwarcać tylko raz
+            player_guess_all = input(f"Would you like to guess the whole word? Y/N: ")
+            if player_guess_all.casefold()[0] == 'y':
+                player_word = input("Your guess: ")
+                if player_word == secret_word:
+                    print("That's right! You win!")
+                    break
+                else:
+                    print("Sorry, you missed! Please try again!")
+            else:
+                print("OK then. Let's carry on!")
+
+        else:
+            print("Sorry, you missed! Please try again!")
+
+
+def draw_hangman():
+    pass
