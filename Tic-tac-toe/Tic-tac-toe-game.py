4b6ea3cbbc68650e64741691d5402bcea84871ac
magdazalewska <m.jarzebska.zalewska@gmail.com>
2022-12-25 23:42:29 +0100

    Added free field validation function

--- Tic-tac-toe/Tic-tac-toe-game.py
+++ Tic-tac-toe/Tic-tac-toe-game.py
@@ -2,8 +2,8 @@
 # losujemy kto zaczyna (x czy o i odpowiednio pytanie o pole) - done
 # komputer losuje pole - done
 # pytamy gracza o pole - done
-# jeśli pole zajęte, to info, że tutaj nie można postawić
 
+# jeśli pole zajęte, to info, że tutaj nie można postawić
 # j.w. - komputer musi postawić na kolejny mwolnym
 # sprawdzamy czy 3 pola obok siebie zajęte
 # jeśli trzy zajęte obok siebie lub wszystkie zajęte, to koniec gry
@@ -12,14 +12,14 @@
 
 import random
 
-print('*' * 38)
+print("*" * 38)
 print("Hello! Welcome to the tic-tac-toe game!")
-print('*' * 38)
+print("*" * 38)
 print('\n')
 
-name = input("Please, state your name: \n")
+name = input("What's your name: \n")
 # game_board = [''] * 9
-game_board = ['', 'X', 'X','X','X', 'X', 'X', 'X', '']
+game_board = ['X', 'X', '','X','', 'X', 'X', 'X', '']
 
 
 def choose_sign():
@@ -27,14 +27,14 @@ def choose_sign():
     while True:
         player_choice = input()
         if player_choice.casefold() not in ['x', 'o']:
-            print("Sorry, this is not a valid choice. Please choose \'X\' or \'O\':\n")
+            print("Sorry, invalid input! Please choose \'X\' or \'O\':\n")
         else:
             if player_choice.casefold() == 'x':
-                print(f"You have chosen \'X\'. I will be \'O\'")
+                print(f"Player 1, you have chosen \'X\'. Player 2 will play as  \'O\'")
                 print("Let\'s start!")
                 return player_choice
             else:
-                print(f"You have chosen \'O\'. I will be \'X\'")
+                print(f"Player 1, you have chosen \'O\'. Player 2 will play as  \'X\'")
                 print("Let\'s start!")
                 return player_choice
 
@@ -45,35 +45,42 @@ def choose_order(player_sign, player_name):
         print(f"{player_name}, you will go fist")
         return order
     else:
-        print("Computer will play first")
+        print("Player 2 will play first")
         return order
 
 
 def draw_board(board):  # tutaj funkcja game
 
-    print('||'.center(17) + '||'.center(3))
-    print(f" {board[0]}".center(8) + '||' + f"{board[1]}".center(9) + '||' + f" {board[2]}".center(8))
-    print('||'.center(17) + '||'.center(3))
+    print('|'.center(17) + '|'.center(3))
+    print(f" {board[0]}".center(8) + '|' + f"{board[1]}".center(9) + '|' + f" {board[2]}".center(8))
+    print('|'.center(17) + '|'.center(3))
     print('---------' * 3)
-    print('||'.center(17) + '||'.center(3))
-    print(f" {board[3]}".center(8) + '||' + f"{board[4]}".center(9) + '||' + f" {board[5]}".center(8))
-    print('||'.center(17) + '||'.center(3))
+    print('|'.center(17) + '|'.center(3))
+    print(f" {board[3]}".center(8) + '|' + f"{board[4]}".center(9) + '|' + f" {board[5]}".center(8))
+    print('|'.center(17) + '|'.center(3))
     print('---------' * 3)
-    print('||'.center(17) + '||'.center(3))
-    print(f" {board[6]}".center(8) + '||' + f"{board[7]}".center(9) + '||' + f" {board[8]}".center(8))
-    print('||'.center(17) + '||'.center(3))
+    print('|'.center(17) + '|'.center(3))
+    print(f" {board[6]}".center(8) + '|' + f"{board[7]}".center(9) + '|' + f" {board[8]}".center(8))
+    print('|'.center(17) + '|'.center(3))
 
 
-def comp_play(comp_sign):  #to nie działa!!
+def comp_play(comp_sign, board):
+    computer_choice = random.randint(0, 8)
+    while True:
+        for index in range(len(board)):
+            if not check_field(board, computer_choice):
+                # computer_choice = random.randint(0, 8)
+                continue
+            else:
+                board[computer_choice] = comp_sign
+                break
 
-    game_on = True
-    while game_on:
-        computer_choice = random.randint(0, 8)
-        if game_board[computer_choice]:
-            pass # co tutaj ma być?
-        else:
-            game_board[computer_choice] += comp_sign
-            game_on = False
+
+def check_field(board, field_index):
+    if not board[field_index]:
+        return True
+    else:
+        return False
 
 
 def game(player_sign):
@@ -91,9 +98,12 @@ def game(player_sign):
                 game_on = False
 
 
-def check_result():
+def check_winner():
+    pass
+
+def chose_game_mode():
     pass
 
 
-comp_play('O')
-draw_board(game_board)
+comp_play('O', game_board)
+print(game_board)
