26ac37cc32ca21e88041e610dcf65e2bb58d7af8
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-01-16 23:25:35 +0100

    Main file - initial commit

--- Simple Text Game/game.py
+++ Simple Text Game/game.py
@@ -1,12 +1,1 @@
-"""
-Napisz prostą grę tekstową. Wymyśl prostą zagadkę, zaprojektuj jedno, max dwa pomieszczenia,
-dodaj możliwość zdobywania i używania przedmiotów. Obsłuż komendy takie jak:
 
-użyj PRZEDMIOT
-zabierz PRZEDMIOT
-przedmioty - wyświetli listę przedmiotów
-pomoc - wyświetli listę dostępnych instrukcji
-opisz - opisuje aktualne pomieszczenie
-
-Dodaj komendy, które uznasz jeszcze za potrzebne. Użyj klas i operacji wejścia/wyjścia.
-"""
