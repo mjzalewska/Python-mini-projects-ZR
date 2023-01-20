825db6de867fb5b2df2a20b84e33c1ceef456358
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-01-21 00:21:22 +0100

    Added Scene class

--- Simple Text Game/classes/scene.py
+++ Simple Text Game/classes/scene.py
@@ -1,3 +1,22 @@
 class Scene:
-    def __init__(self, name):
-        self.scene_name = name
+    def __init__(self, **kwargs):
+        self.no = kwargs['no']
+        self.name = kwargs['name']
+        self.intro = kwargs['intro']
+        self.description = kwargs['description']
+
+        self.items = [item['name'] for item in kwargs['items']]
+
+    def show_intro(self):
+        return self.intro
+
+    def __str__(self):
+        return self.description
+
+    def enumerate_items(self):
+        if len(self.items) > 0:
+            for item in self.items:
+                return item
+        else:
+            return "Nothing in here..."
+
