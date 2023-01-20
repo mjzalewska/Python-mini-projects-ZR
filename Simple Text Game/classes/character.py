d33a82c4962fa6e38471257ed68fb1b9e87a8ed8
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-01-20 22:12:06 +0100

    Added Character class

---
+++ Simple Text Game/classes/character.py
@@ -0,0 +1,34 @@
+# implement use_inventory method
+
+class Character:
+
+    def __init__(self, **kwargs):
+        self.stats = kwargs['stats']
+        self.description = kwargs['description']
+        self.inventory = kwargs['inventory']
+        self.max_inventory = kwargs['max_inventory']
+
+    def __str__(self):
+        return f"{self.description}"
+
+    def add_item(self, item):
+        if len(self.inventory) < self.max_inventory:
+            self.inventory.append(item)
+        else:
+            print("You cannot carry any more items!")
+
+    def remove_item(self, item):
+        try:
+            self.inventory.remove(item)
+        except ValueError:
+            print("Your backpack is empty!")
+
+    def show_inventory(self):
+        if len(self.inventory) > 0:
+            for item in self.inventory:
+                return item
+        else:
+            return "There's nothing in your backpack"
+
+    def use_item(self, item):
+        pass
