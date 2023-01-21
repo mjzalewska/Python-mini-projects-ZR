b314a5cc7773d6c36a7bbfc6470cd8cb0f0d2641
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-01-21 19:01:56 +0100

    Added methods and attributes to the Item class

--- Simple Text Game/classes/item.py
+++ Simple Text Game/classes/item.py
@@ -1,4 +1,19 @@
 class Item:
-    pass
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.description = kwargs['description']
+        self.impact = kwargs['impact']
+        self.user = kwargs['user class']
 
-# inventory properties should go to Item class
+    def __str__(self):
+        return self.description
+
+    def show_item_stats(self):
+        print(f"-{self.name} stats-")
+        for k, v in self.impact.items():
+            print(f"{k}:{v}")
+
+    def boost_char_stats(self, character):
+        for key, value in self.impact.items():
+            character.stats[key] = character.stats[key] + self.impact[key]
+        return character.stats
