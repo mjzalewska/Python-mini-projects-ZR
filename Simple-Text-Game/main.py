d1256c6be40c2caa8fba4034f4ab673a903385ab
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-01-27 00:38:52 +0100

    Added 'take item' and 'use item' method

---
+++ Simple-Text-Game/main.py
@@ -0,0 +1,8 @@
+# show first scene
+# choose to take or leave item - can only take 1 at a time
+# when takes item - solve a puzzle, and got to the second scene
+# in the second scene - if have torch, can drive the goblin away - otherwise fight (choose if fight or drive away)
+# when defeats goblin - the goblin drops a key
+# pass on to the next scene
+# if have the key - can open the gate and run away from the castle, otherwise game over
+
