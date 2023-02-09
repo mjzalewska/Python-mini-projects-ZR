f7f9bdc2331708287402b382fb0d675c61e70258
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-02-09 23:15:20 +0100

    Added __str__ method

--- Simple-Text-Game/classes/character.py
+++ Simple-Text-Game/classes/character.py
@@ -3,6 +3,15 @@ class Character:
     def __init__(self, **kwargs):
         self.stats = kwargs['stats']
         self.description = kwargs['description']
+
+    def __str__(self):
+        return self.description
+
+
+class Hero(Character):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.inventory = kwargs['inventory']
         self.max_inventory = kwargs['max inventory']
         self.weapon = kwargs['weapon type']
@@ -28,3 +37,17 @@ class Character:
                 return item
         else:
             return "There's nothing in your backpack"
+
+
+class Enemy(Character):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.enemy_class = kwargs['class']
+
+    def __str__(self):
+        return self.description
+
+    def show_stats(self):
+        return f"level:{self.stats['level']}\nHP:{self.stats['hp']}\nMP:{self.stats['mp']}\n" \
+               f"ATTACK:{self.stats['attack']}\nDEFENCE{self.stats['defence']}"
