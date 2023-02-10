9d067a03a1c087a9a600e783c5e79376402f51f0
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-02-10 01:38:02 +0100

    Added GamePlay class - first version

--- Simple-Text-Game/classes/game_play.py
+++ Simple-Text-Game/classes/game_play.py
@@ -1,21 +1,25 @@
-import sys
 from file_manager import FileManager
 from item import Item
 from scene import Scene
 from character import Hero
+from time import sleep
 
 
 class GamePlay:
-
-    game_commands = ['H', 'D', 'I', 'T', 'Q']
+    game_state = 'initializing'
+    hero = None
+    current_scene = None
+    available_commands = ['M', 'D', 'I', 'T', 'Q']
 
     @classmethod
-    def show_wellcome_screen(cls): # add greeting
-        print('...')
+    def show_welcome_screen(cls):
+        print()
+        print('You are now in the deserted castle undergrounds')
+        print('Can you find your way out?')
 
     @classmethod
     def show_menu(cls):
-        print('-----GAME MENU-----')
+        print('\n-----GAME MENU-----')
         menu_items = FileManager()
         menu = menu_items.load_json_file(r'..\setup_files\game_menu.json')[0]
         for key, value in menu.items():
@@ -24,48 +28,72 @@ class GamePlay:
     @classmethod
     def choose_character(cls):
         character_types = ['elf', 'mage', 'knight']
-        print('Choose your character: Elf, Mage, Knight ')
+        print('\nChoose your character: Elf, Mage, Knight ')
         while True:
             character_choice = input().casefold()
             if character_choice in character_types:
-                print(f'You have chosen: {character_choice.capitalize()}')
-                return character_choice.capitalize()
+                print(f'You have chosen: {character_choice.capitalize()}\n')
+                return character_choice
             else:
                 print('This is not a valid choice!')
 
     @classmethod
-    def initialize_scene(cls):
-        scene_values = FileManager.load_json_file(r'..\setup_files\scene1.json')
-        current_scene = Scene(**scene_values)
-        return current_scene
+    def initialize(cls):
+        if FileManager.is_scene_available(r'..\setup_files\scene1.json'):
+            cls.show_welcome_screen()
+            scene_values = FileManager.load_json_file(r'..\setup_files\scene1.json')
+            cls.current_scene = Scene(**scene_values)
+            hero_values = FileManager.load_json_file(r'..\setup_files\{}.json'.format(cls.choose_character()))
+            cls.hero = Hero(**hero_values)
+            cls.current_scene.show_intro()
+            cls.show_menu()
 
-    @classmethod
-    def initialize_hero(cls):
-        character_values = FileManager.load_json_file(r'..\setup_files\{}'.format(cls.choose_character()))
-        current_character = Hero(**character_values)
-        return current_character
+            cls.game_state = 'playing'
+        else:
+            if not cls.current_scene.next_scene():
+                print('Congratulations, you have finished the game!')
+                print('Do you want to play again?Y/N')
+                replay = input()
+                if replay[0].casefold() == 'y':
+                    cls.play()
+                else:
+                    exit()
 
     @classmethod
     def play(cls):
         while True:
-            print(f'Available commands: {"".join(cls.game_commands)}')
-            command = input()
-            if command not in cls.game_commands:
-                print('Sorry I don\'t know this command. Please choose again!')
+            if cls.game_state == 'initializing':
+                cls.initialize()
             else:
-                match command:
-                    case 'H':
-                        cls.show_menu()
-                    case 'D':
-                        cls.initialize_scene().__str__()
-                    case 'I':
-                        cls.initialize_hero().show_inventory()
-                    case 'T': #potencjalnie do usunięcia
-                        pass
-                    case 'Q':
-                        break
+                print(f'\nAvailable commands: {",".join(cls.available_commands)}\n')
+                command = input('>> ').capitalize()
+                if command not in cls.available_commands:
+                    print('Sorry I don\'t know this command. Please choose again!')
+                else:
+                    match command: # add show hero stats and show enemy stats
+                        case 'M':
+                            cls.show_menu()
+                        case 'D':
+                            print(cls.current_scene)
+                        case 'I':
+                            cls.hero.show_inventory()
+                        case 'T':
+                            if cls.current_scene.items:
+                                print('Which item would you like to take. You can only take one!')
+                                cls.current_scene.enumerate_items(cls.hero)
+                                item_choice = input().title()
+                                if item_choice in cls.current_scene.items:
+                                    cls.hero.add_item(item_choice)
+                                    cls.current_scene.remove_item(item_choice)
+                                    # when item taken should boost stats
+                                else:
+                                    print('No such item here!')
+                            else:
+                                print('Nothing interesting here...')
+                        case 'Q':
+                            print('Exiting the game...')
+                            sleep(3)
+                            break
 
 
-        pass
-
-
+GamePlay.play()
