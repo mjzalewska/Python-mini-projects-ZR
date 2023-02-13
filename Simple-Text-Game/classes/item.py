e52beac4d43e02690c1683f07ae86783e1d76428
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-02-13 23:55:07 +0100

    Changes the initi method due to changes in the source json file structure

--- Simple-Text-Game/classes/item.py
+++ Simple-Text-Game/classes/item.py
@@ -1,15 +1,15 @@
 class Item:
     def __init__(self, **kwargs):
-        for i in range(len(kwargs['items'])):
-            self.name = kwargs['items'][i]['name']
-            self.description = kwargs['items'][i]['description']
-            self.impact = kwargs['items'][i]['impact']
+        self.name = kwargs.keys()
+        self.type = kwargs['type']
+        self.description = kwargs['description']
+        self.impact = kwargs['impact']
 
     def __str__(self):
         print(self.description)
 
-    def show_item_stats(self):
-        print(f"-{self.name} stats-")
+    def show_item_stats(self, name):
+        print(f"-{name} stats-")
         for k, v in self.impact.items():
             print(f"{k}:{v}")
 
@@ -41,53 +41,3 @@ class Item:
                 print('Sorry there\'s nothing like that in your backpack!')
             else:
                 return item_choice
-
-# from character import Hero
-#
-# values = {
-#     "class": "Elf",
-#     "stats":
-#         {"level": 10,
-#          "hp": 10,
-#          "mp": 8,
-#          "attack": 8,
-#          "defense": 8
-#          },
-#     "weapon type": "Bow",
-#     "max inventory": 6,
-#     "description": "Skilled in swordsmanship and archery. Can use faerie magic and prepare potions",
-#     "inventory": ["Silver Bow"]
-# }
-#
-# item_values = {"items":[
-#   {
-#     "name": "Buster Sword",
-#     "description": "A long steel broadsword, very powerful in close-range combat",
-#     "impact": {
-#       "hp": 0,
-#       "mp": 0,
-#       "attack": 7,
-#       "defense": 3
-#     }
-#   },
-#   {
-#     "name": "Silver Bow",
-#     "description": "A silver emerald-encrusted bow",
-#     "impact": {
-#       "hp": 0,
-#       "mp": 2,
-#       "attack": 4,
-#       "defense": 0
-#     }
-#   }
-#     ]
-# }
-#
-# elf_char = Hero(**values)
-# print(elf_char.stats)
-# item_1 = Item(**item_values)
-# item_1.show_item_stats()
-# elf_char.add_item('Health Potion')
-# elf_char.show_inventory()
-# item_1.boost_char_stats(elf_char)
-# print(elf_char.stats)
