05630645787e7f7de46ad5d69dd4090838db4c89
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-02-13 13:22:18 +0100

    Added enemy attribute and has_enemy method

--- Simple-Text-Game/classes/scene.py
+++ Simple-Text-Game/classes/scene.py
@@ -3,14 +3,19 @@ class Scene:
         self.no = kwargs['no']
         self.name = kwargs['name']
         self.intro = kwargs['intro']
+        self.enemy = kwargs['enemy']
         self.description = kwargs['description']
         self.next_scene = kwargs['next scene']
-
         self.items = [item['name'] for item in kwargs['items']]
 
     def show_intro(self):
         for line in self.intro.split('.'):
             print(line)
+
+    def has_enemy(self):
+        if self.enemy:
+            return True
+        return False
 
     def __str__(self):
         return self.description
