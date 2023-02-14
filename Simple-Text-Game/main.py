4662fc24bf458ee9d48a8cad586df48abe6c2ca3
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-02-14 20:59:14 +0100

    Added program initialitation code

--- Simple-Text-Game/main.py
+++ Simple-Text-Game/main.py
@@ -1,15 +1,4 @@
-# show first scene
-# choose to take or leave item - can only take 1 at a time
-# when takes item - solve a puzzle, and got to the second scene
-# in the second scene - if have torch, can drive the goblin away - otherwise fight (choose if fight or drive away)
-# when defeats goblin - the goblin drops a key
-# pass on to the next scene
-# if have the key - can open the gate and run away from the castle, otherwise game over
-
-"""
-1. load scene and hero file
-2. show menu (add info that the menu can be accessed anytime when H chosen)
-3. show scene and give choice what to do (take item, choose which item to take, what to do, fight or flight)
-4. way to pass to the next scene
-5. repeat
-"""
+# from classes.game_play import GamePlay
+#
+# if __name__ == '__main__':
+#     GamePlay.play()
