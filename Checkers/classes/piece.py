ba18794cea3d976be7920d7b51f220939c2fa5cb
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-03-24 14:39:40 +0100

    Added Piece class

---
+++ Checkers/classes/piece.py
@@ -0,0 +1,28 @@
+class Piece:
+    def __init__(self, color):
+        self.symbol = 'M'
+        self.color = color
+        self.rank = 'man'
+        self.status = 'active'
+        self.position = None
+        self.movement_type = None  # should be a default param, to change for the dame class
+
+    def set_status(self):
+        pass
+
+    def set_position(self):
+        pass
+
+    def set_rank(self):
+        pass
+
+    def set_movement(self):
+        pass
+
+    def set_symmol(self):
+        pass
+
+
+
+
+
