a1e3adb881aa7b808fa54c3ab64d5e98ab6b7000
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-03-26 12:23:27 +0200

    Added helper functions for generating a dict of playable board fields and a dict of promotion line fields

---
+++ Checkers/utilities/helper_functions.py
@@ -0,0 +1,24 @@
+import string
+
+
+# generate a dict of playable board fields (dark only)
+def get_playable_fields():
+    playable_fields = {}
+    for letter in string.ascii_uppercase[:8]:
+        for num in range(2, 10, 2):
+            if letter in ['A', 'C', 'E', 'G']:
+                playable_fields[f'{letter}{num}'] = '_'
+            else:
+                playable_fields[f'{letter}{num - 1}'] = '_'
+    return playable_fields
+
+
+# get promotion line fields
+def get_promotion_line_fields(playable_fields):
+    promotion_line = {}
+    for key, value in playable_fields.items():
+        if key[0] in ['A', 'H']:
+            promotion_line[key] = value
+    return promotion_line
+
+
