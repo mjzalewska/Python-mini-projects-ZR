59caaa41db6461da902047ead3eb2b315178ef59
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-03-28 14:05:32 +0200

    Added display_board method to the Board class

--- Checkers/classes/board.py
+++ Checkers/classes/board.py
@@ -1,18 +1,31 @@
+import string
+
 from Checkers.utilities.helper_functions import get_playable_fields, get_promotion_line_fields
 
 
 class Board:
-    playable_fields = get_playable_fields()
-    promotion_line = get_promotion_line_fields(playable_fields)
+    # p_fields = get_playable_fields()
+    p_fields = {'A2': 'o', 'A4': 'o', 'A6': 'o', 'A8': 'o', 'B1': 'o', 'B3': 'o', 'B5': 'o', 'B7': 'o', 'C2': 'o', 'C4': 'o', 'C6': 'o',
+'C8': 'o', 'D1': ' ', 'D3': ' ', 'D5': ' ', 'D7': ' ', 'E2': ' ', 'E4': ' ', 'E6': ' ', 'E8': ' ', 'F1': 'o', 'F3': 'o',
+'F5': 'o', 'F7': 'o', 'G2': 'o', 'G4': 'o', 'G6': 'o', 'G8': 'o', 'H1': 'o', 'H3': 'o', 'H5': 'o', 'H7': 'o'}
+    promotion_line = get_promotion_line_fields(p_fields)
 
     @classmethod
-    def get_vacant_cells(cls, playable_fields):
-        return {k: v for k, v in playable_fields.items() if not v}
+    def get_vacant_cells(cls, p_fields):
+        return {k: v for k, v in p_fields.items() if not v}
 
     @classmethod
-    def display_board(cls, playable_fields):
-        pass
-
+    def display_board(cls, p_fields):
+        for num in range(1, 9):
+            print(f'   { num}', end="")
+        print(' ')
+        for letter in string.ascii_uppercase[:8]:
+            if letter in ['A', 'C', 'E', 'G']:
+                print(f'{letter}|   | {p_fields[f"{letter}2"]} |   | {p_fields[f"{letter}4"]} |   | '
+                      f'{p_fields[f"{letter}6"]} |   | {p_fields[f"{letter}8"]} |')
+            else:
+                print(f'{letter}| {p_fields[f"{letter}1"]} |   | {p_fields[f"{letter}3"]} |   | '
+                      f'{p_fields[f"{letter}5"]} |   | {p_fields[f"{letter}7"]} |   | ')
 
 
 
