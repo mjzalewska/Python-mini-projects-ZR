986cd6bf8baaea3b92934c18f3adc7e83c9620bb
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-03-30 11:01:37 +0200

    Changed pieces attribute to list and added p_type (player type) as instance attribute

--- Checkers/classes/player.py
+++ Checkers/classes/player.py
@@ -1,15 +1,14 @@
 class Player:
-    def __init__(self):
-        self.type = None
-        self.sign = None
-        self.pieces = 12
+    def __init__(self, p_type):
+        self.type = p_type
+        self.pieces = []
         self.score = 0
         self.dames = 0
 
     def update_pieces_count(self):
         pass
 
-    def update_score(self):
+    def update_score(self): # depends on other player's pieces count, if decrease - score increase
         pass
 
     def update_dames(self):
